e208f040aa4b12a0abb47ac259e5bb2ed027633a
Victor Gimenes <108276273+vhgimenes@users.noreply.github.com>
2023-01-15 15:50:32 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
-# Importando libs externas
-import importlib
-# Importando as funções do pacote
-from .  import connect_tenax_db
-from . import queries_tenax_db
-# Para permitir que mudanças passem a ser usadas diretamente
-importlib.reload(connect_tenax_db)
-importlib.reload(queries_tenax_db)
+__all__ = [connect_db, queries_db]
+
+from .  import connect_db
+from . import queries_db
+
