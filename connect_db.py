98d1488aa9f440c83e2d0dce86f270e4c0f2e898
Victor Gimenes <108276273+vhgimenes@users.noreply.github.com>
2023-01-15 15:49:33 -0300

    Update and rename connect_tenax_db.py to connect_db.py

--- connect_db.py
+++ connect_db.py
@@ -1,31 +1,30 @@
 """
-Autor: Victor Gimenes
-Data: 07/12/2022
+Author: Victor Gimenes
+Date: 07/12/2022
 
-Projeto:
-- Módulo criado com as funçõe que garantem a conexão com o banco de dados da Tenax Capital (SQL Server).
+Módulo criado para armazenar as funções de conexão com bancoS de dados em Azure.
 """
-#Importando os módulos que geram a conexão com o banco
+# Importando os módulos que geram a conexão com o banco
 # Primeira possibilidade: Sqlalchemy
 import sqlalchemy as sa
 # Segunda possibilidade: Pyodbc
 import pyodbc
 
 def get_user():
-    #alterar o usuarios aqui
-    return "tenax-sa"
+    # Alterar o usuário aqui
+    return "insert the username here!"
 
 def get_pass():
-    #alterar a senha aqui
-    return "tp§xM4:2'sRJgQv]MxZn-mk-+y{_)9"
+    # Alterar a senha aqui
+    return "insert the password here!"
 
 def get_string_connection():
-    #alterar o driver aqui
-    driver = "{ODBC Driver 18 for SQL Server}"
-    #alterar o server aqui
-    server = "tcp:tenax.database.windows.net"
-    #alterar o database aqui
-    database = 'tenax-db'
+    # Alterar o driver aqui
+    driver = "insert the driver name here!"
+    # Alterar o server aqui
+    server = "insert the server name here!"
+    # Alterar o database aqui
+    database = 'insert the database name here!'
     username = get_user()
     password = get_pass()
     return "Driver={0};Server={1};Database={2};UID={3};PWD={4};PORT=1433;".format(driver, server, database, username, password)
@@ -35,27 +34,27 @@ def get_string_connection_sa():
       
 def get_connection():
     """
-    Função que retorna a conexão com o banco de dados da Tenax. 
+    Função que retorna a conexão com o banco de dados via pyodbc. 
     
-    Para se poder utilizar a função, é necessário instalar o driver ODBC Driver 18 for SQL Server (x64, não necessariamente) no Link:
+    É necessário instalar o driver ODBC Driver 18 for SQL Server no Link:
     https://docs.microsoft.com/pt-br/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16
     
     Obs.: sempre que usar a função, lembrar de fechar a conexão com o banco.
     
     Returns:
-        obj: conexão com o banco de dados da Tenax Capital.
+        obj: conexão com o banco de dados.
     """
     connection = pyodbc.connect(get_string_connection())
     return connection
 
 def get_connection_sa():
     """
-    Função que retorna a conexão com o banco de dados da Tenax via sqlalchemy. 
+    Função que retorna a conexão com o banco de dados via sqlalchemy (melhor para o uso via pandas). 
     
     Obs.: sempre que usar a função, lembrar de fechar a conexão com o banco.
     
     Returns:
-        obj: conexão com o banco de dados da Tenax Capital.
+        obj: conexão com o banco de dados.
     """
     engine = sa.create_engine(get_string_connection_sa())
     connection = engine.connect()
